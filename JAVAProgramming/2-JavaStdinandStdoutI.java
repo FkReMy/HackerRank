4624e1d0e7c050c58879a639bc00d11dbdf6e831
FkReMy <fekryjunior@gmail.com>
2024-02-20 21:48:58 +0200

    Create 2-JavaStdinandStdoutI.java

---
+++ JAVAProgramming/2-JavaStdinandStdoutI.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int Intx = scanner.nextInt();
+        int Inty = scanner.nextInt();
+        int Intz = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println(Intx);
+        System.out.println(Inty);
+        System.out.println(Intz);
+
+    }
+}
