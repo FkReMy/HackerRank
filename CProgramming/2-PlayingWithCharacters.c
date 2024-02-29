dab973c3a077ff0916607c3f263a44b53aa09342
FkReMy <54580955+FkReMy@users.noreply.github.com>
2024-02-29 19:56:53 +0200

    Update 2-PlayingWithCharacters.c

--- CProgramming/2-PlayingWithCharacters.c
+++ CProgramming/2-PlayingWithCharacters.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include <stdio.h>   // Include standard input/output library
+#include <string.h>  // Include string manipulation library
+#include <math.h>    // Include math library (not used in this example)
+#include <stdlib.h>  // Include standard library (not used in this example)
 
+int main() {
+    char ch;          // Declare a single character variable   
+    char ca[100];     // Declare a character array to store strings up to 99 characters
+    char s[100];      // Another character array for string storage
 
-int main() 
-{
-    char ch ;
-    char ca[100];
-    char s[100];
-    
-    scanf("%c\n", &ch);
-    scanf("%[^\n]%*c", ca);
-    scanf("%[^\n]%*c", s);
-    
-    printf("%c\n", ch);
-    printf("%s\n", ca);
-    printf("%s\n", s);
-    
-    return 0;
+    scanf("%c\n", &ch);  // Read a single character and consume the newline
+
+    scanf("%[^\n]%*c", ca);  // Read an entire line of input (up to a newline) into 'ca' 
+
+    scanf("%[^\n]%*c", s);   // Read another entire line of input (up to a newline) into 's'
+
+    printf("%c\n", ch);   // Print the single character
+    printf("%s\n", ca);   // Print the first input line
+    printf("%s\n", s);    // Print the second input line
+
+    return 0;             // Indicate successful program execution
 }
