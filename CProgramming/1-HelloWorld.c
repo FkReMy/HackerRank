359a65a6efd14955a1475fb10f9fa7a2c9c968a7
FkReMy <54580955+FkReMy@users.noreply.github.com>
2024-03-03 22:47:04 +0200

    Update 1-HelloWorld.c

--- CProgramming/1-HelloWorld.c
+++ CProgramming/1-HelloWorld.c
@@ -6,7 +6,7 @@
 int main() {
     char s[100];      // Declare a character array to store a string of up to 99 characters
 
-    scanf("%[^\n]%c", s);  // Read a string from the user until a newline is encountered 
+    scanf("%[^\n]c", s);  // Read a string from the user until a newline is encountered 
 
     printf("Hello, World!\n%s", s);  // Print "Hello, World!" followed by the entered string
 
